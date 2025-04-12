c201af8b288b8096fbdde63e03b139e84a5aba9a
hennong <henning.koenigs@gmx.de>
2025-04-12 23:31:38 +0200

    Add nativewind to mobile app
    - rename `mobile` -> `mobile-app`

---
+++ applications/mobile-app/app/index.tsx
@@ -0,0 +1,16 @@
+import "../css/global.css";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-xl">Styled with tailwind</Text>
+    </View>
+  );
+}
